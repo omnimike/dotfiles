3db34436be244861bef92a042daa33fe137986ee
Michael Fulthorp <michael.fulthorp@learnosity.com>
2016-08-05 12:43:41 +1000

    save

--- zsh/cmd.zsh
+++ zsh/cmd.zsh
@@ -11,3 +11,26 @@ fda() {
   local dir
   dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
 }
+
+# fe - edit file
+fe() {
+    local file
+    file=$(fzf +m) || return
+    e $file
+}
+
+# fco - checkout git branch/tag
+fco() {
+  local tags branches target
+  tags=$(
+    git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
+  branches=$(
+    git branch --all | grep -v HEAD             |
+    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+    sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+  target=$(
+    (echo "$tags"; echo "$branches") |
+    fzf --no-hscroll --ansi +m -d "\t" -n 2) || return
+  git checkout $(echo "$target" | awk '{print $2}')
+}
+
