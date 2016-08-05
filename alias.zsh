5930a58056c8e2112acfa368c4bd6d085df5e914
Michael Fulthorp <michael.fulthorp@learnosity.com>
2016-08-05 10:59:47 +1000

    save

--- alias.zsh
+++ alias.zsh
@@ -3,11 +3,22 @@
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
 #
+alias e="vim"
+alias r='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
+
+# config edits
 alias zshrc="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
 alias zshreload="source ~/.zshrc"
 alias zshalias="vim ~/.oh-my-zsh/custom/alias.zsh"
 alias vimrc="vim ~/.vimrc"
-alias e="vim"
-alias code="cd ~/vg/code"
-alias r='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
+
+# places
+alias gcode="cd ~/vg/code"
+alias gquestions="cd ~/vg/code/api/questionsV2"
+alias gquestioneditor="cd ~/vg/code/api/questioneditor"
+alias gschemas="cd ~/vg/code/api/schemas"
+alias gauthor="cd ~/vg/code/api/author"
+alias gdocs="cd ~/vg/code/site/docs"
+alias gdemos="cd ~/vg/code/site/demos"
+
