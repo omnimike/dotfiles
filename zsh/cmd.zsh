e43c6a43d6201814becc03496087b0a6fa9270b3
Michael Fulthorp <michael.fulthorp@learnosity.com>
2016-08-05 12:07:21 +1000

    save

---
+++ zsh/cmd.zsh
@@ -0,0 +1,13 @@
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-*} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+# fda - including hidden directories
+fda() {
+  local dir
+  dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
+}
