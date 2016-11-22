2f4ac501d1e6ec3bccfe159d0fbac1b52148d969
Michael Fulthorp <michael.fulthorp@learnosity.com>
2016-11-22 12:43:38 +1100

    save

--- zsh/cmd.zsh
+++ zsh/cmd.zsh
@@ -1,49 +1,4 @@
-# alias
-alias e='subl'
-alias r='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
-alias j='jump'
-alias g='git'
-alias vg='vagrant'
 
-alias zshrc="vim ~/.zshrc"
-alias zshreload="source ~/.zshrc"
-alias vimrc="vim ~/.vimrc"
-
-# variables
-export FZF_DEFAULT_COMMAND='ag -g ""'
-export PATH="$HOME/bin:$PATH"
-export EDITOR='vim'
-
-# plugins
-source ~/conf/zsh/antigen.zsh
-
-antigen use oh-my-zsh
-
-# oh-my-zsh plugins
-antigen bundle git
-antigen bundle osx
-antigen bundle sublime
-antigen bundle vagrant
-antigen bundle command-not-found
-
-#other plugins
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle jocelynmallon/zshmarks
-antigen bundle supercrabtree/k
-
-antigen apply
-
-# theme
-antigen theme pure
-
-# vim keybindings
-bindkey -v
-
-if [ -e ~/.zshrc-local ]; then
-    source ~/.zshrc-local
-fi
-
-# Custom functions
 # fd - cd to selected directory
 fd() {
   local dir
@@ -84,4 +39,3 @@ fco() {
 fh() {
   print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
-
