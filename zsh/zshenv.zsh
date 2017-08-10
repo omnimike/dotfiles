c98fb15b0f0ea6a71bce371aa06e88bc250ba9e1
Michael Fulthorp <michael.fulthorp@learnosity.com>
2017-08-10 11:06:53 +1000

    save

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -9,7 +9,7 @@ local ANACONDA_PATH="$HOME/uni/da/anaconda3/bin"
 local TEX_PATH="/Library/TeX/texbin"
 local GO_PATH="/usr/local/opt/go/libexec/bin:$HOME/go/bin"
 local YARN_PATH="/Users/michael/.config/yarn/global/node_modules/.bin"
-export PATH="$HOME/bin:$YARN_PATH:$GO_PATH:$ANACONDA_PATH:$TEX_PATH:$PATH"
+export PATH="$PATH:$HOME/bin:$YARN_PATH:$GO_PATH:$ANACONDA_PATH:$TEX_PATH"
 export GOPATH="$HOME/go"
 export EDITOR='vim'
 
