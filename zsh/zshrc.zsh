d930012a00c6f0f717946c85b81e63d9576aa85b
Michael Fulthorp <michael.fulthorp@learnosity.com>
2017-08-11 10:53:41 +1000

    save

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,3 +1,11 @@
+
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+
 # alias
 alias e='vim'
 alias r='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
