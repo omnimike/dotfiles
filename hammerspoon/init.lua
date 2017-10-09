4cc77d3d9cf429d386a1c0660d41fab1b26de038
Michael Fulthorp <michael.fulthorp@learnosity.com>
2017-10-09 12:16:47 +1100

    save

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -15,11 +15,9 @@ local appHotkeys = {
     { 'p', 'Google Play Music Desktop Player' },
 }
 
-local hyperMap = hs.hotkey.modal.new()
 
 local hyperBind = function(key, fn)
     hs.hotkey.bind(hyper, key, nil, fn)
-    hyperMap:bind({}, key, nil, fn)
 end
 
 for i, binding in ipairs(appHotkeys) do
@@ -38,5 +36,40 @@ end)
 hs.hotkey.bind({}, 'F13', function ()
     hs.reload()
 end)
+
+hs.window.animationDuration = 0
+hs.grid.setGrid('2x2')
+
+hyperBind('a', function ()
+    local win = hs.window.focusedWindow()
+    win:moveOneScreenWest()
+end)
+
+hyperBind('g', function ()
+    local win = hs.window.focusedWindow()
+    win:moveOneScreenEast()
+end)
+
+local gridHotkeys = {
+    { 'w', '0,0 1x1' },
+    { 'e', '0,0 2x1' },
+    { 'r', '1,0 1x1' },
+    { 's', '0,0 1x2' },
+    { 'd', '0,0 2x2' },
+    { 'f', '1,0 1x2' },
+    { 'x', '0,1 1x1' },
+    { 'c', '0,1 2x1' },
+    { 'v', '1,1 1x1' },
+}
+
+for i, binding in ipairs(gridHotkeys) do
+    local key = binding[1]
+    local pos = binding[2]
+    local launcher = function()
+        local win = hs.window.focusedWindow()
+        hs.grid.set(win, pos)
+    end
+    hyperBind(key, launcher)
+end
 
 hs.alert.show('conf reloaded')
