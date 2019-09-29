6da5c27022434402682933e6c4134fbbcd0a3961
Michael Fulthorp <michaelf@fb.com>
2019-09-28 20:24:10 -0700

    save

--- vim/install.sh
+++ vim/install.sh
@@ -3,15 +3,17 @@
 set -euo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
+
 mkdir -p "$HOME/.vim"
 ln -svf "$DIR/init.vim" "$HOME/.vimrc"
+mkdir -p "$HOME/.vim/autoload"
+ln -svf "$DIR/plug.vim" "$HOME/.vim/autoload/plug.vim"
+
 mkdir -p "$HOME/.vim-temp/backup"
 mkdir -p "$HOME/.vim-temp/swapfiles"
 mkdir -p "$HOME/.vim-temp/undo"
 
-curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+# setup neovim to use the same config as vim
 mkdir -p "$HOME/.config"
 if [[ ! -L "$HOME/.config/nvim" ]]; then
     ln -svf "$HOME/.vim" "$HOME/.config/nvim"
