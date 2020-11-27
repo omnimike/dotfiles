bd3ae9f8be03847172e45263f4e0311994fa2f12
Michael Fulthorp <michaelf@fb.com>
2020-11-26 20:43:16 -0800

    save

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,7 +12,7 @@ end)
 local appHotkeys = {
     { '\\', 'Google Chrome' },
     { '\'', 'iTerm' },
-    { '0', 'Workplace Chat' },
+    { '/', 'Workplace Chat' },
     { '-', 'Mail' },
     { '=', 'Calendar' },
 }
