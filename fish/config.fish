7f2e33d95acfb2f3cd62c0a1170ee58ea9146a49
Michael F <michael.fulthorp@gmail.com>
2022-10-08 18:34:39 +0800

    save

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
+fish_add_path ~/dotfiles/bin
+fish_add_path ~/.cargo/bin
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
     fish_vi_key_bindings
-    fish_add_path ~/dotfiles/bin
     set -Ux EDITOR nvim
 
     alias e nvim
