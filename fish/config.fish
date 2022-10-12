1c62be02b46c99dcc0405b7f752f15078d985ff8
Michael Fulthorp <michael.fulthorp@gmail.com>
2022-10-12 18:23:46 +0800

    save

--- fish/config.fish
+++ fish/config.fish
@@ -10,4 +10,17 @@ if status is-interactive
     alias e nvim
     alias fishrc "e ~/.config/fish/config.fish"
     alias j to
+    alias tm tmux
+    alias tm 'tmux attach || tmux new'
+    alias gpb 'git push origin $(git_current_branch)'
+    alias gpbf 'git push origin $(git_current_branch) --force-with-lease'
+
+    alias vimrc "e ~/.vimrc"
+    alias vimrcl "e ~/.vimrc-local"
+    alias vimrclp "e ~/.vimrc-local-plugins"
+    alias tmuxrc "e ~/.tmux.conf"
+    alias tmuxrcl "e ~/.tmux-local.conf"
+    alias hammerspoonrc "e ~/.hammerspoon/init.lua"
+    alias karabinerrc "e ~/.config/karabiner/karabiner.json"
+    alias sshconfig "e ~/.ssh/config"
 end
