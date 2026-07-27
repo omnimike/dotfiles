54726de32b1565f5ad61f8f535cbf6425f9556bb
Michael Fulthorp <michael.fulthorp@gmail.com>
2026-07-27 00:16:03 -0700

    [bash] Added command timer to prompt

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -38,5 +38,36 @@ alias tmuxrcl="e ~/.tmux-local.conf"
 alias hammerspoonrc="e ~/.hammerspoon/init.lua"
 alias sshconfig="e ~/.ssh/config"
 
-export PROMPT_COMMAND=""
-export PS1='\n\[\e[0;33m\]\t\[\e[m\] \[\e[0;34m\]\h\[\e[m\] \[\e[0;32m\]\w\[\e[m\]\n\[\e[0;35m\]\$\[\e[m\] '
+timer_show=""
+
+__bash_timer_start() {
+  timer=${timer:-$SECONDS}
+}
+
+__bash_timer_stop() {
+  if [ -n "${timer+x}" ]; then
+    local i=$(($SECONDS - $timer))
+    local days=$((i / 86400))
+    local hours=$(( (i / 3600) % 24 ))
+    local mins=$(( (i / 60) % 60 ))
+    local secs=$((i % 60))
+    timer_show=""
+    if [ "$days" -gt 0 ]; then timer_show="${timer_show}${days}d "; fi
+    if [ "$hours" -gt 0 ]; then timer_show="${timer_show}${hours}h "; fi
+    if [ "$mins" -gt 0 ]; then timer_show="${timer_show}${mins}m "; fi
+    if [ "$secs" -gt 0 ]; then timer_show="${timer_show}${secs}s"; fi
+  fi
+  unset timer
+}
+
+trap '__bash_timer_start' DEBUG
+
+__bash_precmd() {
+  __bash_timer_stop
+  local timer_part=""
+  [ -n "$timer_show" ] && timer_part=" \[\e[0;33m\]${timer_show}\[\e[m\]"
+
+  PS1="\n\[\e[0;33m\]\t\[\e[m\] \[\e[0;34m\]\h\[\e[m\] \[\e[0;32m\]\w\[\e[m\]${timer_part}\n\[\e[0;35m\]\$\[\e[m\] "
+}
+
+export PROMPT_COMMAND=__bash_precmd
