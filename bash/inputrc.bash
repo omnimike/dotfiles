cc989d171bb56671c4bb880f4b3eb05d30220172
Michael Fulthorp <michael.fulthorp@gmail.com>
2026-07-27 01:11:34 -0700

    [bash] Added vi mode and improved key seq timeout

--- bash/inputrc.bash
+++ bash/inputrc.bash
@@ -1,5 +1,14 @@
-
+set editing-mode vi
 set show-mode-in-prompt on
 set vi-cmd-mode-string "\1\e[2 q\2"
 set vi-ins-mode-string "\1\e[6 q\2"
-set keyseq-timeout 50
+set keyseq-timeout 500
+
+set keymap vi-command
+"ciw": "lbcw"
+"diw": "lbdw"
+"yiw": "lbyw"
+"ciW": "lBcW"
+"diW": "lBdW"
+"yiW": "lByW"
+
