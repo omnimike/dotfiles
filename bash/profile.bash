75ae4e8d79fcddb4bf51519503f3aad392fc395f
Michael Fulthorp <michael.fulthorp@gmail.com>
2026-08-09 01:34:34 -0700

    [bash] [zsh] Updated Homebrew path to /opt/homebrew/bin for Apple Silicon

--- bash/profile.bash
+++ bash/profile.bash
@@ -2,7 +2,7 @@
 
 DOT_PATH="$HOME/.dotfiles/bin"
 LOCAL_PATH="$HOME/.local/bin"
-BREW_PATH="/usr/local/bin"
+BREW_PATH="/opt/homebrew/bin"
 OPENCODE_PATH="$HOME/.opencode/bin"
 
 export PATH="$PATH:$HOME/bin:$DOT_PATH:$BREW_PATH:$LOCAL_PATH:$OPENCODE_PATH"
--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,7 +1,7 @@
 
 local DOT_PATH="$HOME/.dotfiles/bin"
 local LOCAL_PATH="$HOME/.local/bin"
-local BREW_PATH="/usr/local/bin"
+local BREW_PATH="/opt/homebrew/bin"
 local OPENCODE_PATH="$HOME/.opencode/bin"
 
 export PATH="$PATH:$HOME/bin:$DOT_PATH:$BREW_PATH:$LOCAL_PATH:$OPENCODE_PATH"
