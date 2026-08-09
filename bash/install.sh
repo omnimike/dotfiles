2469bbe523a5e45b319349236c4b19b1919f2312
Michael Fulthorp <michaelf@meta.com>
2026-08-08 23:42:14 -0700

    [bash] Source bashrc from login shells

--- bash/install.sh
+++ bash/install.sh
@@ -4,24 +4,47 @@ set -euo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
-if [[ ! -f "$HOME/.bashrc" ]]; then
-  echo "creating dummy .bashrc that sources bashrc.bash"
-  echo "[[ -f $DIR/bashrc.bash ]] && source $DIR/bashrc.bash" > "$HOME/.bashrc"
-elif ! grep -q "bashrc.bash" "$HOME/.bashrc"; then
-  echo "adding bashrc.bash to .bashrc"
-  echo "[[ -f $DIR/bashrc.bash ]] && source $DIR/bashrc.bash" >> "$HOME/.bashrc"
+if [[ ! -f "$HOME/.bash_profile" ]]; then
+  echo "creating ~/.bash_profile that sources ~/.profile"
+  {
+    echo "[[ -f $HOME/.profile ]] && source $HOME/.profile"
+  } > "$HOME/.bash_profile"
 else
-  echo "bashrc.bash already sourced in .bashrc"
+  echo ".bash_profile already exists"
 fi
 
 if [[ ! -f "$HOME/.profile" ]]; then
-  echo "creating dummy .profile that sources profile.bash"
-  echo "[[ -f $DIR/profile.bash ]] && source $DIR/profile.bash" > "$HOME/.profile"
-elif ! grep -q "profile.bash" "$HOME/.profile"; then
-  echo "adding profile.bash to .profile"
-  echo "[[ -f $DIR/profile.bash ]] && source $DIR/profile.bash" >> "$HOME/.profile"
+  echo "creating ~/.profile that sources ~/.bashrc"
+  {
+    echo "[[ -f $DIR/profile.bash ]] && source $DIR/profile.bash"
+    echo "[[ -f $HOME/.bashrc ]] && source $HOME/.bashrc"
+  } > "$HOME/.profile"
 else
-  echo "profile.bash already sourced in .profile"
+  if ! grep -q "profile.bash" "$HOME/.profile"; then
+    echo "adding profile.bash to ~/.profile"
+    echo "[[ -f $DIR/profile.bash ]] && source $DIR/profile.bash" >> "$HOME/.profile"
+  else
+    echo "profile.bash already sourced in ~/.profile"
+  fi
+  if ! grep -q "\.bashrc" "$HOME/.profile"; then
+    echo "adding ~/.bashrc to ~/.profile"
+    echo "[[ -f $HOME/.bashrc ]] && source $HOME/.bashrc" >> "$HOME/.profile"
+  else
+    echo "~/.bashrc already sourced in ~/.profile"
+  fi
+fi
+
+if [[ ! -f "$HOME/.bashrc" ]]; then
+  echo "creating ~/.bashrc that sources bashrc.bash"
+  {
+    echo '[[ $- != *i* ]] && return'
+    echo "[[ -f $DIR/bashrc.bash ]] && source $DIR/bashrc.bash"
+  } > "$HOME/.bashrc"
+elif ! grep -q "bashrc.bash" "$HOME/.bashrc"; then
+  echo "adding bashrc.bash to ~/.bashrc"
+  echo "[[ -f $DIR/bashrc.bash ]] && source $DIR/bashrc.bash" >> "$HOME/.bashrc"
+else
+  echo "bashrc.bash already sourced in ~/.bashrc"
 fi
 
 ln -svf "$DIR/inputrc.bash" "$HOME/.inputrc"
