bad6bb150228d4c618ae4cbeb310d21245fbf863
Michael Fulthorp <michaelf@meta.com>
2026-08-08 23:44:12 -0700

    [bash] Remove invalid local declarations from profile

--- bash/profile.bash
+++ bash/profile.bash
@@ -1,8 +1,8 @@
 
 
-local DOT_PATH="$HOME/.dotfiles/bin"
-local LOCAL_PATH="$HOME/.local/bin"
-local BREW_PATH="/usr/local/bin"
-local OPENCODE_PATH="$HOME/.opencode/bin"
+DOT_PATH="$HOME/.dotfiles/bin"
+LOCAL_PATH="$HOME/.local/bin"
+BREW_PATH="/usr/local/bin"
+OPENCODE_PATH="$HOME/.opencode/bin"
 
 export PATH="$PATH:$HOME/bin:$DOT_PATH:$BREW_PATH:$LOCAL_PATH:$OPENCODE_PATH"
