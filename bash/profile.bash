bd9bffd9e0a0ec135c7a17ea94f0b9f401d10330
Michael Fulthorp <michael.fulthorp@gmail.com>
2026-08-09 01:37:21 -0700

    [bash] [zsh] Make the system path lower priority

--- bash/profile.bash
+++ bash/profile.bash
@@ -5,4 +5,4 @@ LOCAL_PATH="$HOME/.local/bin"
 BREW_PATH="/opt/homebrew/bin"
 OPENCODE_PATH="$HOME/.opencode/bin"
 
-export PATH="$PATH:$HOME/bin:$DOT_PATH:$BREW_PATH:$LOCAL_PATH:$OPENCODE_PATH"
+export PATH="$HOME/bin:$DOT_PATH:$BREW_PATH:$LOCAL_PATH:$OPENCODE_PATH:$PATH"
--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -4,4 +4,4 @@ local LOCAL_PATH="$HOME/.local/bin"
 local BREW_PATH="/opt/homebrew/bin"
 local OPENCODE_PATH="$HOME/.opencode/bin"
 
-export PATH="$PATH:$HOME/bin:$DOT_PATH:$BREW_PATH:$LOCAL_PATH:$OPENCODE_PATH"
+export PATH="$HOME/bin:$DOT_PATH:$BREW_PATH:$LOCAL_PATH:$OPENCODE_PATH:$PATH"
