8e930e3a5c46dc7c0253bba9e4c757c9915e62c1
michaelf <michaelf@fb.com>
2026-08-08 23:03:12 -0700

    [bash] Create dummy bashrc and profile if they don't exist

--- bash/install.sh
+++ bash/install.sh
@@ -4,14 +4,20 @@ set -euo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
-if [[ $(grep -q "bashrc.bash" "$HOME/.bashrc"; echo $?) -eq 1 ]]; then
+if [[ ! -f "$HOME/.bashrc" ]]; then
+  echo "creating dummy .bashrc that sources bashrc.bash"
+  echo "[[ -f $DIR/bashrc.bash ]] && source $DIR/bashrc.bash" > "$HOME/.bashrc"
+elif ! grep -q "bashrc.bash" "$HOME/.bashrc"; then
   echo "adding bashrc.bash to .bashrc"
   echo "[[ -f $DIR/bashrc.bash ]] && source $DIR/bashrc.bash" >> "$HOME/.bashrc"
 else
   echo "bashrc.bash already sourced in .bashrc"
 fi
 
-if [[ $(grep -q "profile.bash" "$HOME/.profile"; echo $?) -eq 1 ]]; then
+if [[ ! -f "$HOME/.profile" ]]; then
+  echo "creating dummy .profile that sources profile.bash"
+  echo "[[ -f $DIR/profile.bash ]] && source $DIR/profile.bash" > "$HOME/.profile"
+elif ! grep -q "profile.bash" "$HOME/.profile"; then
   echo "adding profile.bash to .profile"
   echo "[[ -f $DIR/profile.bash ]] && source $DIR/profile.bash" >> "$HOME/.profile"
 else
